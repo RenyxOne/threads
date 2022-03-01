04fc69d1639370c50916ab76d4b678ccb65a17a0
Ruslan Shamsutdinov <Ruslan_Shamsutdinov@epam.com>
2022-03-01 14:44:14 +0300

    done

--- main.cpp
+++ main.cpp
@@ -3,124 +3,72 @@
 #include <ctime>
 #include <list>
 #include <thread>
+#include <windows.h>
+#include <atomic>
 
-void printMatrix(int **matrix, int n, int m){
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            std::cout << matrix[i][j] << '\t';
-        }
-        std::cout << std::endl;
+int counterForMutex = 0;
+int counterForCommon = 0;
+std::atomic<int> counterForAtomic = 0;
+
+std::mutex door;
+int threadCount = std::thread::hardware_concurrency();
+
+void incCounterMutex(int count){
+    std::lock_guard<std::mutex> t(door);
+    for (int i = 0; i < count; i++) {
+        counterForMutex++;
     }
 }
 
-//void calc
-
-int** createMatrixWithZeros (int n, int m) {
-    int** matrix = new int*[n];
-    for (int i = 0; i < n; i++) {
-        matrix[i] = new int[m];
-        for (int j = 0; j < m; j++){
-            matrix[i][j] = 0;
-        }
-    }
-    return matrix;
-}
-
-void calcOneElement(int **A, int **B, int size, int **dest, int i, int j){
-    int res = 0;
-    for (int k = 0; k < size; k++){
-        res += A[i][k] * B[k][j];
-    }
-    dest[i][j] = res;
-}
-
-void calcOneResultLine(int **A, int **B, int size, int **dest, int i) {
-    for (int j = 0; j < size; j++){
-        int res = 0;
-        for (int k = 0; k < size; k++){
-            res += A[i][k] * B[k][j];
-        }
-        dest[i][j] = res;
+void incCounterAtomic(int count){
+    for (int i = 0; i < count; i++) {
+        counterForAtomic++;
     }
 }
 
-void pow2MatrixAsync(int** A, int** B, int n2, int m1, int** dest){
+void incCounterCommon(int count){
+    for (int i = 0; i < count; i++) {
+        counterForCommon++;
+    }
+}
+
+template <class T>
+void testCounter(int incCount, void(*func)(int count), T& counter, const char* str) {
+    counter = 0;
+    int size = (threadCount > incCount) ? 1 : incCount / threadCount;
+
     std::list<std::thread*> threadList;
-    for (int i = 0; i < n2; i++){
-        threadList.push_front(new std::thread(calcOneResultLine, A, B, n2, dest, i));
-//        for (int j = 0; j < m1; j++){
-//            threadList.push_front(new std::thread(calcOneElement, A, B, n2, dest, i, j));
-//        }
+    int maxThreads = threadCount < incCount ? threadCount : incCount;
+
+    for (int i = 0; i < maxThreads; i++){
+         threadList.push_front(new std::thread(func, size));
     }
+
     for (auto t : threadList){
         t->join();
         delete t;
     }
-
-}
-
-void pow2MatrixSync(int** A, int** B, int n2, int m1, int** dest){
-    for (int i = 0; i < n2; i++){
-        calcOneResultLine(A,B,m1,dest, i);
-//        for (int j = 0; j < m1; j++){
-//            calcOneElement(A, B, n2, dest, i, j);
-//        }
-    }
-}
-
-void testMults(int size){
-    std::cout << "Test myltiply: to square matrix with size " << size << std::endl;
-    int** matrix = createMatrixWithZeros(size,size);
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++){
-            matrix[i][j] = rand() % 10;
-        }
-    }
-
-    std::cout << "Original matrix:" << std::endl;
-    printMatrix(matrix,size,size);
-
-    int** result = createMatrixWithZeros(size,size);
-    pow2MatrixAsync(matrix, matrix, size, size, result);
-
-    std::cout << "matrix^2:" << std::endl;
-    printMatrix(result, size, size);
-
-
-    std::cout << "Test finished" << std::endl << std::endl;
-}
-
-void testTime (int size) {
-    int n = size;
-    int m = size;
-    int** matrix = createMatrixWithZeros(n,m);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++){
-            matrix[i][j] = rand();
-        }
-    }
-
-    int** result = createMatrixWithZeros(n,m);
-    //sync mult
-    time_t syncStart = clock();
-    pow2MatrixSync(matrix, matrix, n, m, result);
-    time_t syncEnd = clock();
-
-    //async mult
-    time_t asyncStart = clock();
-    pow2MatrixAsync(matrix, matrix, n, m, result);
-    time_t asyncEnd = clock();
-
-    time_t syncTime = syncEnd - syncStart;
-    time_t asyncTime = asyncEnd - asyncStart;
-
-    std::cout << "Size matrix: " << n << "x" << m << std::endl;
-    std::cout << "syncTime: " << syncTime << std::endl;
-    std::cout << "asyncTime: " << asyncTime << std::endl;
+    std::cout << str << std::endl;
+    std::cout << "theory: " << incCount << std::endl;
+    std::cout << "real: " << counter << std::endl;
+    std::cout << std::endl;
 }
 
 int main() {
-    testMults(2);
-    testTime(750);
-
+//    testMults(2);
+//    testTime(1000);
+    int count = 1000000;
+    clock_t start, end;
+    start = clock();
+    testCounter(threadCount * count, incCounterAtomic, counterForAtomic, "atomic");
+    end = clock();
+    std::cout << end - start << std::endl;
+    start = clock();
+    testCounter(threadCount * count, incCounterMutex, counterForMutex, "Mutex");
+    end = clock();
+    std::cout << end - start << std::endl;
+    start = clock();
+    testCounter(threadCount * count, incCounterCommon, counterForCommon, "Common");
+    end = clock();
+    std::cout << end - start << std::endl;
 }
